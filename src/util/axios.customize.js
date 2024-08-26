9909fd4494d68f345b3f10df724a8d42eeae1cb2
tuan-anh-83 <tuananh83cbt@gmail.com>
2024-08-26 17:32:00 +0700

    Step 2 - Fix Cors

--- src/util/axios.customize.js
+++ src/util/axios.customize.js
@@ -25,6 +25,7 @@ instance.interceptors.response.use(
   function (response) {
     // Any status code that lie within the range of 2xx cause this function to trigger
     // Do something with response data
+    if (response && response.data) return response.data;
     return response;
   },
   function (error) {
